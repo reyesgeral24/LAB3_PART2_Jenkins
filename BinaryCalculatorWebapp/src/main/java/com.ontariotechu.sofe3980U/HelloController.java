bd8c02bfe267e65b4b38e61c3e75bb5f74b0860c
reyesgeral24 <reyesgeral24@gmail.com>
2026-03-09 17:40:13 -0400

    Update HelloController.java

--- BinaryCalculatorWebapp/src/main/java/com.ontariotechu.sofe3980U/HelloController.java
+++ BinaryCalculatorWebapp/src/main/java/com.ontariotechu.sofe3980U/HelloController.java
@@ -1,7 +1,6 @@
 package com.ontariotechu.sofe3980U;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,11 +8,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
-	@GetMapping("/hello")
-	public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		
-		model.addAttribute("name", name);
-		
-		return "hello";
-	}
+    @GetMapping("/hello")
+    public String home(
+            @RequestParam(name="name", required=false, defaultValue="World") String name,
+            Model model) {
+
+        model.addAttribute("name", name);
+
+        return "hello";
+    }
 }
